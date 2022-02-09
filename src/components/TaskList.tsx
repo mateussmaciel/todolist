96b7870a5e7af9b0c62b9b3251b272f687e73e1e
Mateus <mateus.fmaciel@uni9.edu.br>
2022-02-08 21:56:55 -0300

    desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    if(tasks.find(task => task.title == newTaskTitle))
+      return window.alert("O título deve ser diferente de um já utilizado!");
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task )
+
+      setTasks(task);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskRemove = tasks.filter(task => task.id !== id);
+    setTasks(taskRemove);
   }
 
   return (
